ae08f3a41d5fbbbacd8c1465a45c40af91e1c8a6
Ritik Raj <ssinghritik810@gmail.com>
2026-03-21 10:21:27 +0530

    url update

--- api/index.js
+++ api/index.js
@@ -16,7 +16,9 @@ connectDB();
 const app = express();
 
 const corsOptions = {
-  origin: ["http://localhost:5173"],
+  origin: ["http://localhost:5173",
+    "https://stay-ease07.vercel.app"
+  ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
   allowedHeaders: ["Content-Type", "Authorization"],
